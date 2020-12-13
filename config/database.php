61b5b3cf873359be69ea9d4b7b1e4c3b6e7be1fe
王时雨 <1790802712@qq.com>
2020-12-13 19:03:14 +0800

    fix:编辑首页

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'http://47.115.200.182',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'zm_chat',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'zaq1XSW@',
+    'password'        => 'root',
     // 端口
     'hostport'        => 3306,
     // 连接dsn
